d9b77d99ea030ee99c8b773fbfd85a245b7bad9f
mohamycom <yasser.hawawi@hotmail.com>
2025-06-08 14:38:46 +0300

    Add files via upload

--- mohamy_bot.py
+++ mohamy_bot.py
@@ -5,8 +5,13 @@ import os
 
 TOKEN = os.environ.get("BOT_TOKEN")
 
+WELCOME_MESSAGE = (
+    "مرحبا بكم في مساعدكم (محامي.كوم) وهو بوت متخصص في تقديم الاستشارات القانونية الاحترافية التي تساعدك على فهم حقوقك واتخاذ القرار القانوني الصحيح بثقة.\n\n"
+    "لا نحتاج الى اي معلومات شخصية (مثل الاسم او العنوان او رقم الهاتف ...إلخ) وسيتم حذف الاستشارات تلقائيا من السيرفرات فور انتهاء المحادثة فلا داعي للقلق حيال ذلك."
+)
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("مرحبًا بك في بوت المحامي! كيف يمكنني مساعدتك؟")
+    await update.message.reply_text(WELCOME_MESSAGE)
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"لقد قلت: {update.message.text}")
