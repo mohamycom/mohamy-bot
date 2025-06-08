b36aab9bd1aa77e6052b08bedd26c593a7cd3b7d
mohamycom <yasser.hawawi@hotmail.com>
2025-06-08 13:09:33 +0300

    Add files via upload

--- mohamy_bot.py
+++ mohamy_bot.py
@@ -1,11 +1,23 @@
-import telebot
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-bot = telebot.TeleBot(BOT_TOKEN)
+TOKEN = os.environ.get("BOT_TOKEN")
 
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    bot.reply_to(message, "أهلاً بك في بوت الاستشارات القانونية.")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("مرحبًا بك في بوت المحامي! كيف يمكنني مساعدتك؟")
 
-bot.infinity_polling()
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"لقد قلت: {update.message.text}")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
